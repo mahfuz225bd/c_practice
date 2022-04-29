9e5aeebaa8e518e99587af9eff33699866e957ec
mahfuz <mahfuz01bd@gmail.com>
2022-04-29 21:52:47 +0600

    leap_year_checker.c added

---
+++ leap_year_checker.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+  int year, isLeapYear;
+  
+  isLeapYear = 0;
+
+  while (1) {
+    printf("Enter year: ");
+    scanf("%d", &year);
+
+    if (year % 4 == 0) {
+      if (year % 100 == 0) {
+        if (year % 400 == 0) {
+          isLeapYear = 1;
+        } else {
+          isLeapYear = 0;
+        }
+      }
+    } else {
+      isLeapYear = 0;
+    }
+
+    if (isLeapYear) {
+      printf("%d is a leap year.\n", year);
+    } else {
+      printf("%d is not a leap year.\n", year);
+    }
+  }
+
+  return 0;
+}
