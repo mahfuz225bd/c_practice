c578106d014deaac57c188b87854dbc8982f52c6
mahfuz <mahfuz01bd@gmail.com>
2022-04-30 11:11:43 +0600

    13. get_random_number.c added

---
+++ 13. get_random_number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int get_random_number;
+    time_t t;
+
+    srand((unsigned)time(&t));
+
+    get_random_number = rand() % 100 + 1;
+
+    printf("Random Number: %d", get_random_number);
+
+    return 0;
+}
+
+/* Resources:
+- time_t - https://en.cppreference.com/w/c/chrono/time_t
+- time.h - https://www.tutorialspoint.com/c_standard_library/c_function_time.htm
+- srand and rand - https://www.geeksforgeeks.org/rand-and-srand-in-ccpp/
+*/
